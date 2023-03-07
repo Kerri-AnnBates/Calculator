8e85dc1004b6eac4e8d5e95c392b875edb09a10f
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-03-06 21:43:13 -0500

    update display

--- scripts/modules/CalculatorUI.js
+++ scripts/modules/CalculatorUI.js
@@ -8,6 +8,7 @@ class CalculatorUI {
         this.currNumber = 0;
         this.operator = null;
         this.calc = new Calculator(this.prevNumber, this.currNumber);
+        this.initDisplay();
         this.events();
     }
 
@@ -16,25 +17,54 @@ class CalculatorUI {
     }
 
     execute(e) {
-        // console.log(e);
-        const key = e.target.dataset.key;
         const isNumber = e.target.className.includes("number");
         const isOperator = e.target.className.includes("operator");
         const isAction = e.target.className.includes("option");
         const isDecimal = e.target.id == "decimal";
+        const key = e.target.dataset.key;
+
 
         if (isNumber || isDecimal) {
             console.log("I'm a number. Or decimal");
+
+            // Update numbers in screen
+            this.updateDisplay(key, isDecimal);
+            this.currNumber = this.convertIfNumber(this.displayScreen.value);
         }
 
         if (isOperator) {
             console.log("I'm an operator");
+            console.log("Current number is", this.currNumber);
+            console.log("Previous number is", this.prevNumber);
         }
 
         if (isAction) {
             console.log("I'm an action");
         }
+    }
 
+    initDisplay() {
+        this.displayScreen.value = this.currNumber;
+    }
+
+    convertIfNumber(value) {
+        if (isNaN(value)) {
+            return value;
+        }
+
+        return parseFloat(value);
+    }
+
+    updateDisplay(key, isDecimal) {
+        if (this.displayScreen.value === "0") {
+            if (isDecimal) {
+                this.displayScreen.value += key;
+            } else {
+                this.displayScreen.value = key;
+            }
+        } else {
+            this.displayScreen.value += key;
+        }
     }
 }
 
