27bc51b1222422a193cbeffac6dd430bf2cc0855
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-03-06 20:35:44 -0500

    got buttons ready for some logic

--- scripts/modules/CalculatorUI.js
+++ scripts/modules/CalculatorUI.js
@@ -3,8 +3,10 @@ import Calculator from "./Calculator.js";
 class CalculatorUI {
     constructor() {
         this.buttons = document.querySelectorAll('button');
+        this.displayScreen = document.querySelector("#screen");
         this.prevNumber = 0;
         this.currNumber = 0;
+        this.operator = null;
         this.calc = new Calculator(this.prevNumber, this.currNumber);
         this.events();
     }
@@ -14,7 +16,25 @@ class CalculatorUI {
     }
 
     execute(e) {
-        console.log(e.target);
+        // console.log(e);
+        const key = e.target.dataset.key;
+        const isNumber = e.target.className.includes("number");
+        const isOperator = e.target.className.includes("operator");
+        const isAction = e.target.className.includes("option");
+        const isDecimal = e.target.id == "decimal";
+
+        if (isNumber || isDecimal) {
+            console.log("I'm a number. Or decimal");
+        }
+
+        if (isOperator) {
+            console.log("I'm an operator");
+        }
+
+        if (isAction) {
+            console.log("I'm an action");
+        }
+
     }
 }
 
