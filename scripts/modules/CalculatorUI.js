c863abdaac719b3d4866556c312d21db58f79b5d
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-03-16 14:08:29 -0400

    fix calculations

--- scripts/modules/CalculatorUI.js
+++ scripts/modules/CalculatorUI.js
@@ -7,6 +7,7 @@ class CalculatorUI {
         this.prevNumber = 0;
         this.currNumber = 0;
         this.operator = null;
+        this.prevOperator = this.operator;
         this.operatorSelected = false;
         this.calc = new Calculator(this.prevNumber, this.currNumber, 0, this.operator);
         this.initDisplay();
@@ -25,7 +26,7 @@ class CalculatorUI {
         const key = e.target.dataset.key;
 
         if (isNumber || isDecimal) {
-            console.log("I'm a number. Or decimal");
+            // console.log("I'm a number. Or decimal");
 
             // Update numbers in screen
             this.updateDisplay(key, isDecimal);
@@ -33,7 +34,7 @@ class CalculatorUI {
         }
 
         if (isOperator) {
-            console.log("I'm an operator", key);
+            // console.log("I'm an operator", key);
             console.log("Current number is", this.currNumber);
             console.log("Previous number is", this.prevNumber);
 
@@ -78,29 +79,26 @@ class CalculatorUI {
 
     handleOperatorClick(key) {
         this.operatorSelected = true;
+        this.prevOperator = this.operator;
+        this.operator = key;
+        console.log("Previous operator", this.prevOperator);
 
         // first time selecting operator
         if (this.calc.operator == null) {
             this.calc.operator = key;
             this.handleOperatorChange(key, this.currNumber);
         } else {
-            // Check if we're just switching operator
-            if (this.calc.operator !== key) {
-                console.log("Changed operator");
-                this.handleOperatorChange(key, this.prevNumber);
-            } else {
-                this.calc.num1 = this.prevNumber;
-                this.calc.num2 = this.currNumber;
-            }
+            // TODO: Handle change of operator
 
-            this.calc.operator = key;
-            // this.calc.num1 = this.prevNumber;
-            // this.calc.num2 = this.currNumber;
+            this.calc.operator = this.prevOperator;
+            this.calc.num1 = this.prevNumber;
+            this.calc.num2 = this.currNumber;
         }
 
-        this.prevNumber = this.calc.calculate();
-        this.displayScreen.value = this.prevNumber;
 
+        this.prevNumber = this.calc.calculate();
+        this.displayScreen.value = this.calc.total;
+        this.prevOperator = key;
     }
 
     handleOperatorChange(operator, savedNumber) {
